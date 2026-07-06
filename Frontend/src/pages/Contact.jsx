3fc162a0bdcf22c69b0cdfe198c27e368af19d7a
jnolly <japhethanold03@gmail.com>
2026-07-06 20:53:29 +0300

    V3

--- Frontend/src/pages/Contact.jsx
+++ Frontend/src/pages/Contact.jsx
@@ -143,13 +143,22 @@ export default function Contact() {
             {/* Map */}
             <section className="max-w-[1200px] mx-auto px-5 md:px-16 mb-20">
                 <div className="bg-[#e9e1dc] rounded-2xl overflow-hidden shadow-inner h-96 relative border border-[#e0bfbf]">
-                    <div className="absolute inset-0 opacity-40 bg-cover bg-center">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12413.613858528197!2d34.34668999538447!3d0.4787334552250803!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x177f830035fa558b%3A0xc93490a4bde2271c!2sSt%20Michael%20madende%20catholic%20church!5e0!3m2!1sen!2ske!4v1783343948700!5m2!1sen!2ske" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>
+                    <div className="absolute inset-0 bg-cover bg-center">
+                     <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12413.613858528197!2d34.34668999538447!3d0.4787334552250803!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x177f830035fa558b%3A0xc93490a4bde2271c!2sSt%20Michael%20madende%20catholic%20church!5e0!3m2!1sen!2ske!4v1783343948700!5m2!1sen!2ske"
+                        width="100%"
+                        height="450"
+                        style={{ border: 0 }}
+                        allowFullScreen
+                        loading="lazy"
+                        referrerPolicy="strict-origin-when-cross-origin"
+                        title="St. Michael Madende Catholic Church Location"
+                     />
                     </div>
-                    <div className="absolute inset-0 flex items-center justify-center">
-                        <div className="bg-white p-4 rounded-xl shadow-xl flex items-center gap-3 border border-[#e0bfbf] animate-bounce">
+                    <div className="absolute inset- flex items-center justify-right p-35">
+                        <div className="bg-white p-4 rounded-xl shadow-xl flex items-center gap-3 border border-[#e0bfbf]">
                             <span className="material-symbols-outlined text-[#570013] text-3xl">location_on</span>
-                            <div>
+                            <div>                                                     
                                 <p className="font-bold text-[#570013]">St. Michael Madende</p>
                                 <p className="text-caption text-[#584141]">View on Google Maps</p>
                             </div>

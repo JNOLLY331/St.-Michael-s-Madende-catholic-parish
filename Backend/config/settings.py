24b9f06b9964a3c733e8f302916e4498b9b692b1
jnolly <japhethanold03@gmail.com>
2026-07-07 09:47:08 +0300

    V4

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -157,6 +157,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5174",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://st-michael-s-madende-catholic-paris-gilt.vercel.app"
 ]
 
 #DRF and rest_Framework
